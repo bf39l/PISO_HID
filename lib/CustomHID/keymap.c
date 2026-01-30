c50bc85fac9ea89670621a285828005864707c15
bf39L <boliu1234@gmail.com>
2026-01-30 17:03:36 +1300

    Update keymap again

--- lib/CustomHID/keymap.c
+++ lib/CustomHID/keymap.c
@@ -17,11 +17,11 @@ uint32_t keymaps[MAX_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
     // --- Layer 1: Function ---
     {
         {
-            KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F12,      KC_TRNS, KC_F6,   KC_F7,                KC_F8,   KC_F9,                 KC_F10,  KC_F11,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, MT(MD_LALT, KC_LEFT), KC_UP,   MT(MD_LALT, KC_RIGHT), KC_TRNS, KC_TRNS,
-            KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_LEFT,              KC_DOWN, KC_RIGHT,              KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, MT(MD_LGUI, KC_LEFT), KC_TRNS, MT(MD_LGUI, KC_RIGHT), KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_DELETE,   KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS
+            KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   MT(MD_LCTRL | MD_LSHIFT | MD_LALT | MD_LGUI, KC_ESC),      KC_F12,    KC_F6, KC_F7,   KC_F8,    KC_F9,    KC_F10,    KC_F11,
+            KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRACKET,                                               KC_BSLASH, KC_Y,  KC_U,    KC_UP,    KC_O,     KC_P,      KC_EQUAL,
+            KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRACKET,                                               KC_QUOTE,  KC_H,  KC_LEFT, KC_DOWN,  KC_RIGHT, KC_SCOLON, KC_ENTER,
+            KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                                          KC_N,  KC_M,    KC_COMMA, KC_DOT,   KC_SLASH,  KC_RSHIFT,
+            KC_LCTRL,  KC_LALT, KC_LGUI,                   KC_TRNS, KC_DELETE,                                                 TG(2),     KC_TRNS, KC_RGUI, KC_RALT, KC_RCTRL
         }
     },
     // --- Layer 2: Config ---
@@ -31,7 +31,7 @@ uint32_t keymaps[MAX_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
             KC_NO,   KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_NO,   KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_NO,   KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,                        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO
+            KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,      TG(2),   KC_NO, KC_NO, KC_NO, KC_NO
         }
     },
     // --- Layer 0: Base ---
